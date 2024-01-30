dfea4405571396eaa8a19aeb081c7d0eb21d5679
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-01-30 17:49:12 +0530

    Create sieve of eratosthenes.cpp

---
+++ Day30/Primes/sieve of eratosthenes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int prime(int n)
+{
+    vector<bool>jyo(n+1,true);
+    jyo[0]=false;
+    jyo[1]=false;
+    for(int i=2;i<=int(sqrt(n));i++)
+    {
+        for(int j=i*i;j<=n;j+=i)
+        {
+            jyo[j]=false;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+		if (jyo[i])
+			cout << i << " ";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    prime(n);
+}
