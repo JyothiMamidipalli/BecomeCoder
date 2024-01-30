7603583438f82d8ed6917033e6a19026601ebd39
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-01-30 18:09:50 +0530

    Create Sieve II.cpp

---
+++ Prime Number/Sieve II.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<bool>jyo(n+1,true);
+    jyo[0]=false;
+    jyo[1]=false;
+    for(int i=2;i<=int(sqrt(n));i++)
+    {
+        if(jyo[i])
+        {
+            for(int j=i*i;j<=n;j+=i)
+            {
+                jyo[j]=false;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+		if (jyo[i])
+			cout << i << " ";
+    }
+}
+
+//T.C-->O(n*log(log(n)))
